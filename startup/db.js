2693ad132593fd10d2dbda8985fc4447dda7ff88
augustine-awuori <codewithaugustine@gmail.com>
2024-05-25 00:53:39 +0300

    Show DB conn error

--- startup/db.js
+++ startup/db.js
@@ -4,5 +4,8 @@ import winston from "winston";
 export default function () {
   mongoose
     .connect(process.env.db)
-    .then(() => winston.info(`Connection to database is successful!`));
-};
+    .then(() => winston.info(`Connection to database is successful!`))
+    .catch((error) =>
+      winston.error(`Error connecting to the databse ${error}`)
+    );
+}
