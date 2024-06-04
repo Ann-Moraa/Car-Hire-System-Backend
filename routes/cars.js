be675646e000d5411352b1dc27b9652f6e534ba8
augustine-awuori <codewithaugustine@gmail.com>
2024-06-04 15:30:57 +0300

    Register a car

---
+++ routes/cars.js
@@ -0,0 +1,36 @@
+import express from "express";
+
+import { Car, validateCar } from "../models/car.js";
+import auth from "../middlewares/auth.js";
+import validator from "../middlewares/validate.js";
+
+const router = express.Router();
+
+router.post("/", [auth, validator(validateCar)], async (req, res) => {
+  const { fuel, mileage, model, plate, selfDrive, type, year, images } =
+    req.body;
+
+  const car = new Car({
+    fuel,
+    lessee: req.user._id,
+    mileage,
+    model,
+    images,
+    plate,
+    selfDrive,
+    type,
+    year,
+  });
+
+  await car.save();
+
+  res.send(car);
+});
+
+router.get("/", async (_req, res) => {
+  const cars = await Car.find({});
+
+  res.send(cars);
+});
+
+export default router;
