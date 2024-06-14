8053e9b9aeb7c47d63987cd86011304f9cfca915
augustine-awuori <codewithaugustine@gmail.com>
2024-06-14 13:52:07 +0300

    Test

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const winston = require("winston");
 const express = require("express");
 const app = express();
 
-require("./startup/logging")();
-require("./startup/routes")(app);
-require("./startup/db")();
-require("./startup/config")();
-require("./startup/prod")(app);
+// require("./startup/logging")();
+// require("./startup/routes")(app);
+// require("./startup/db")();
+// require("./startup/config")();
+// require("./startup/prod")(app);
+
+console.log("Working");
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () =>
