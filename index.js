4276dbd154ff7ec220411623c26a88ad3984453f
augustine-awuori <codewithaugustine@gmail.com>
2024-06-14 13:54:44 +0300

    Test: Add logger

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const winston = require("winston");
 const express = require("express");
 const app = express();
 
-// require("./startup/logging")();
+const logging = require("./startup/logging");
 // require("./startup/routes")(app);
 // require("./startup/db")();
 // require("./startup/config")();
 // require("./startup/prod")(app);
 
 console.log("Working");
+logging();
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () =>
